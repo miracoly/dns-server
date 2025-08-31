f0fbaa8eea616fa920717fee1ce6b022d8e04f9c
miracoly <68049792+miracoly@users.noreply.github.com>
2025-08-31 20:22:14 +0200

    feat: echo request

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <arpa/inet.h>
 #include <log.h>
 #include <netinet/in.h>
 #include <stdarg.h>
@@ -8,6 +9,7 @@
 #include <unistd.h>
 
 #define PORT 2053
+#define BUF_SIZE 512
 
 int main(void) {
   int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -21,7 +23,7 @@ int main(void) {
   addr.sin_addr.s_addr = htonl(INADDR_ANY);
   addr.sin_port = htons(PORT);
 
-  if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) != 0) {
+  if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) != 0) {
     log_fatal("could not bind to port");
     close(sockfd);
     exit(EXIT_FAILURE);
@@ -29,5 +31,34 @@ int main(void) {
 
   log_info("starting dns server, listening on port 2053");
 
+  while (true) {
+    struct sockaddr_in clientaddr;
+    socklen_t clientlen = sizeof(clientaddr);
+    char buf[BUF_SIZE + 1];
+    struct sockaddr* pclientaddr = (struct sockaddr*)&clientaddr;
+
+    ssize_t n_read =
+        recvfrom(sockfd, buf, BUF_SIZE, 0, pclientaddr, &clientlen);
+    if (n_read < 0) {
+      log_error("could not read from socket");
+      continue;
+    }
+
+    char ip[INET_ADDRSTRLEN];
+    if (inet_ntop(AF_INET, &clientaddr.sin_addr, ip, sizeof(ip)) == NULL) {
+      log_error("invalid client ip address");
+      continue;
+    }
+    buf[n_read] = '\0';
+    log_info("request from %s: %s", ip, buf);
+
+    ssize_t n_sent = sendto(sockfd, buf, n_read, 0, pclientaddr, clientlen);
+    if (n_sent < 0) {
+      log_error("could not send to socket");
+      continue;
+    }
+  }
+
+  close(sockfd);
   return 0;
 }
